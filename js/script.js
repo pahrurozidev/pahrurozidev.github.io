9121f46199a3720969e26b86d75e0a60a6f13617
Pahrurozi <pahrurozi17@gmail.com>
2021-08-20 20:04:35 +0800

    u

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
+const ul = document.querySelector('ul');
+const li = ul.getElementsByTagName('li');
 
 menuToggle.addEventListener('click', function () {
     nav.classList.toggle('slide');
+    nav.classList.replace('slideBack', 'slide');
+
 });
+
+for (list of li) {
+    list.addEventListener('click', function () {
+        menuToggle.checked = false;
+        nav.classList.toggle('slideBack');
+    });
+}
