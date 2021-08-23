0fd6f0249dd784bb9b0ca8adef9cc96f1919937d
Pahrurozi <pahrurozi17@gmail.com>
2021-08-23 23:51:01 +0800

    u

--- js/script.js
+++ js/script.js
@@ -20,3 +20,38 @@ const personalDetails = document.querySelectorAll('.personal-details .list ul');
 const lastUl = personalDetails[1];
 const lastLi = lastUl.getElementsByTagName('li')[5];
 lastLi.classList.add('job');
+
+const span3 = document.getElementsByTagName('span')[3];
+const span4 = document.getElementsByTagName('span')[4];
+const span5 = document.getElementsByTagName('span')[5];
+const span6 = document.getElementsByTagName('span')[6];
+const span7 = document.getElementsByTagName('span')[7];
+const span8 = document.getElementsByTagName('span')[8];
+const span9 = document.getElementsByTagName('span')[9];
+const span10 = document.getElementsByTagName('span')[10];
+
+setInterval(() => {
+
+    setTimeout(() => {
+        span3.classList.add('large');
+        span4.classList.add('large');
+        span5.classList.add('large');
+        span6.classList.add('large');
+        span7.classList.add('large');
+        span8.classList.add('large');
+        span9.classList.add('large');
+        span10.classList.add('large');
+    }, 1000);
+
+    setTimeout(() => {
+        span3.classList.remove('large');
+        span4.classList.remove('large');
+        span5.classList.remove('large');
+        span6.classList.remove('large');
+        span7.classList.remove('large');
+        span8.classList.remove('large');
+        span9.classList.remove('large');
+        span10.classList.remove('large');
+    }, 2000);
+
+}, 2000);
