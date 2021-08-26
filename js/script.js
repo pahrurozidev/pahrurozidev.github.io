92b2a4c380c94262a307b836e0664056cccd44e3
Pahrurozi <pahrurozi17@gmail.com>
2021-08-26 09:43:47 +0800

    up

--- js/script.js
+++ js/script.js
@@ -21,43 +21,61 @@ const lastUl = personalDetails[1];
 const lastLi = lastUl.getElementsByTagName('li')[5];
 lastLi.classList.add('job');
 
-const span3 = document.getElementsByTagName('span')[3];
-const span4 = document.getElementsByTagName('span')[4];
-const span5 = document.getElementsByTagName('span')[5];
-const span6 = document.getElementsByTagName('span')[6];
-const span7 = document.getElementsByTagName('span')[7];
-const span8 = document.getElementsByTagName('span')[8];
-const span9 = document.getElementsByTagName('span')[9];
-const span10 = document.getElementsByTagName('span')[10];
+// const span3 = document.getElementsByTagName('span')[3];
+// const span4 = document.getElementsByTagName('span')[4];
+// const span5 = document.getElementsByTagName('span')[5];
+// const span6 = document.getElementsByTagName('span')[6];
+// const span7 = document.getElementsByTagName('span')[7];
+// const span8 = document.getElementsByTagName('span')[8];
+// const span9 = document.getElementsByTagName('span')[9];
+// const span10 = document.getElementsByTagName('span')[10];
 
+// setInterval(() => {
+
+//     setTimeout(() => {
+//         span3.classList.add('large');
+//         span4.classList.add('large');
+//         span5.classList.add('large');
+//         span6.classList.add('large');
+//         span7.classList.add('large');
+//         span8.classList.add('large');
+//         span9.classList.add('large');
+//         span10.classList.add('large');
+//     }, 1000);
+
+//     setTimeout(() => {
+//         span3.classList.remove('large');
+//         span4.classList.remove('large');
+//         span5.classList.remove('large');
+//         span6.classList.remove('large');
+//         span7.classList.remove('large');
+//         span8.classList.remove('large');
+//         span9.classList.remove('large');
+//         span10.classList.remove('large');
+//     }, 2000);
+
+// }, 2000);
+
+// $(window).scroll(() => {
+//     const wScroll = $(this).scrollTop();
+//     $('.header-profile').css({
+//         'transform': 'translate(0, ' + wScroll + 'px)'
+//     });
+// });
+
+const bodyBox = document.getElementsByClassName('body-box')[0];
+const box = bodyBox.querySelectorAll('.box');
 setInterval(() => {
-
     setTimeout(() => {
-        span3.classList.add('large');
-        span4.classList.add('large');
-        span5.classList.add('large');
-        span6.classList.add('large');
-        span7.classList.add('large');
-        span8.classList.add('large');
-        span9.classList.add('large');
-        span10.classList.add('large');
+        box[2].classList.remove('jingga');
+        box[0].classList.add('jingga');
     }, 1000);
-
     setTimeout(() => {
-        span3.classList.remove('large');
-        span4.classList.remove('large');
-        span5.classList.remove('large');
-        span6.classList.remove('large');
-        span7.classList.remove('large');
-        span8.classList.remove('large');
-        span9.classList.remove('large');
-        span10.classList.remove('large');
+        box[0].classList.remove('jingga');
+        box[1].classList.add('jingga');
     }, 2000);
-
-}, 2000);
-
-
-const liAll = document.querySelector('#contact .container ul li');
-const li3 = liAll.nextElementSibling.nextElementSibling;
-const textarea = li3.getElementsByTagName('textarea')[0];
-textarea.setAttribute('class', 'textareaTablet');
+    setTimeout(() => {
+        box[1].classList.remove('jingga');
+        box[2].classList.add('jingga');
+    }, 3000);
+}, 3000);
