f3bbf710a1300f68317ec563abc7d0c2e3832a5e
Pahrurozi <pahrurozi17@gmail.com>
2021-08-27 08:54:13 +0800

    u

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// * slidback navbar
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 const ul = document.querySelector('ul');
@@ -15,54 +16,26 @@ for (list of li) {
     });
 }
 
+// * button read more
 const personalDetails = document.querySelectorAll('.personal-details .list ul');
-
 const lastUl = personalDetails[1];
 const lastLi = lastUl.getElementsByTagName('li')[5];
 lastLi.classList.add('job');
 
-// const span3 = document.getElementsByTagName('span')[3];
-// const span4 = document.getElementsByTagName('span')[4];
-// const span5 = document.getElementsByTagName('span')[5];
-// const span6 = document.getElementsByTagName('span')[6];
-// const span7 = document.getElementsByTagName('span')[7];
-// const span8 = document.getElementsByTagName('span')[8];
-// const span9 = document.getElementsByTagName('span')[9];
-// const span10 = document.getElementsByTagName('span')[10];
+const span = document.getElementsByTagName('span');
+for (let i = 0; i < span.length; i++) {
+    setInterval(() => {
+        setTimeout(() => {
+            span[i].classList.add('large');
+        }, 1000);
 
-// setInterval(() => {
+        setTimeout(() => {
+            span[i].classList.remove('large');
+        }, 2000);
+    }, 2000);
+}
 
-//     setTimeout(() => {
-//         span3.classList.add('large');
-//         span4.classList.add('large');
-//         span5.classList.add('large');
-//         span6.classList.add('large');
-//         span7.classList.add('large');
-//         span8.classList.add('large');
-//         span9.classList.add('large');
-//         span10.classList.add('large');
-//     }, 1000);
-
-//     setTimeout(() => {
-//         span3.classList.remove('large');
-//         span4.classList.remove('large');
-//         span5.classList.remove('large');
-//         span6.classList.remove('large');
-//         span7.classList.remove('large');
-//         span8.classList.remove('large');
-//         span9.classList.remove('large');
-//         span10.classList.remove('large');
-//     }, 2000);
-
-// }, 2000);
-
-// $(window).scroll(() => {
-//     const wScroll = $(this).scrollTop();
-//     $('.header-profile').css({
-//         'transform': 'translate(0, ' + wScroll + 'px)'
-//     });
-// });
-
+// * toggle before login
 const bodyBox = document.getElementsByClassName('body-box')[0];
 const box = bodyBox.querySelectorAll('.box');
 setInterval(() => {
@@ -79,3 +52,32 @@ setInterval(() => {
         box[2].classList.add('jingga');
     }, 3000);
 }, 3000);
+
+// * image from api with async await
+const searchButton = document.querySelector('#search-button');
+searchButton.addEventListener('click', async function () {
+    const inputKeyword = document.querySelector('#search-input');
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies)
+})
+// * getMovie
+function getMovies(keyword) {
+    return fetch('http://www.omdbapi.com/?apikey=487eb934&s=' + keyword)
+        .then(response => response.json())
+        .then(response => response.Search)
+}
+// * updateUI
+function updateUI(movies) {
+    let cards = '';
+    movies.forEach(m => cards += showCards(m));
+    const movieContainer = document.querySelector('#galery .container');
+    movieContainer.innerHTML = cards;
+}
+// * showCards
+function showCards(m) {
+    return `<div class="card">
+                <div class="header">
+                    <img src="` + m.Poster + `" alt="">
+                </div>
+            </div>`
+}
